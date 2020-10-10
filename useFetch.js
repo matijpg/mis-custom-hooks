06b6e048b09751e80a2488e0511d8e3fe350082f
Matias <hernanmatiassierra@gmail.com>
2020-10-10 13:10:22 -0300

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,52 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useFetch = (url) => {
+
+   const isMounted = useRef(true);
+
+   const [state, setState] = useState({
+
+      data: null,
+      loading: true,
+      error: null,
+
+   });
+
+
+   useEffect(() => {
+
+      return () => {
+         isMounted.current = false;
+      }
+
+   }, []);
+
+   useEffect(() => {
+
+      setState({
+         error: null,
+         loading: true,
+         data: null,
+      })
+
+      fetch(url)
+         .then((resp) => resp.json())
+         .then((data) => {
+
+            if (isMounted.current) {
+               setState({
+                  error: null,
+                  loading: false,
+                  data: data,
+               })
+            } else {
+               console.log(('setState no se llamo'));
+            }
+
+         })
+
+   }, [url]);
+
+   return state;
+
+}
---
+++ useForm.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+export const useForm = (initialState = {}) => {
+
+   const [formValues, setFormValues] = useState(initialState);
+
+   const reset = () => {
+      setFormValues(initialState)
+   }
+
+   const handleChange = ({ target }) => {
+
+      setFormValues({
+         ...formValues,
+         [target.name]: target.value
+      });
+
+   }
+
+   return [
+      formValues,
+      handleChange,
+      reset,
+   ]
+
+}
